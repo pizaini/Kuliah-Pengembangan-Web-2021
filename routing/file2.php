7fd0934e7e46d881974ebffa152d3cc6add849ef
Pizaini <zheiner.taurus@gmail.com>
2021-04-01 15:11:05 +0700

    routing sederhana

---
+++ routing/file2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by Pizaini <pizaini@uin-suska.ac.id>
+ * Date: 01/04/2021
+ * Time: 15:06
+ */
+echo 'ini dari file2.php';
---
+++ routing/file3.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by Pizaini <pizaini@uin-suska.ac.id>
+ * Date: 01/04/2021
+ * Time: 15:09
+ */
+?>
+<h1>ini dari file3</h1>
